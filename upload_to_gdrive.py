668c2209653e4cb47f548c746fb9a68fcb96c9d4
Matheus Guimaraes <matheusguimr@gmail.com>
2020-09-02 14:38:01 -0300

    Add loop to restart script if google auth fail

--- upload_to_gdrive.py
+++ upload_to_gdrive.py
@@ -8,116 +8,98 @@ from dateutil.relativedelta import relativedelta
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-# AM: Login to Google Drive and create drive object
-gauth = GoogleAuth()
+while True:
+    try:
+        # AM: Login to Google Drive and create drive object
+        gauth = GoogleAuth()
 
-# Automatic autentication
-# AM: Try to load saved client credentials
-gauth.LoadCredentialsFile("mycreds.txt")
+        # Automatic autentication
+        # AM: Try to load saved client credentials
+        gauth.LoadCredentialsFile("mycreds.txt")
 
-if gauth.credentials is None:
-    # Authenticate if they're not there
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
-else:
-    # Initialize the saved creds
-    gauth.Authorize()
-# Save the current credentials to a file
-gauth.SaveCredentialsFile("mycreds.txt")
+        if gauth.credentials is None:
+            # Authenticate if they're not there
+            gauth.LocalWebserverAuth()
+        elif gauth.access_token_expired:
+            # Refresh them if expired
+            gauth.Refresh()
+        else:
+            # Initialize the saved creds
+            gauth.Authorize()
+        # Save the current credentials to a file
+        gauth.SaveCredentialsFile("mycreds.txt")
 
-# End Automatic autentication
-drive = GoogleDrive(gauth)
-gauth.LocalWebserverAuth()
+        # End Automatic autentication
+        drive = GoogleDrive(gauth)
+        gauth.LocalWebserverAuth()
 
-if len(sys.argv) < 4:
-    print('Usage: gotoDrive.py '
-          '[source dir] '
-          '[file type] '
-          '[gdrive folder id]')
+        if len(sys.argv) < 4:
+            print('Usage: upload_to_gdrive.py '
+                  '[source dir] '
+                  '[file type] '
+                  '[gdrive folder id]')
+            print('Example: python upload_to_gdrive.py'
+                  ' images/ jgp 12lp12l4p')
+            break
 
-pathDirSource = sys.argv[1]  # 'sourceDir'
-extension_files = sys.argv[2]  # 'mp4'
-id_drive = sys.argv[3]  # 12lp12l4p125k12o215k
+        pathDirSource = sys.argv[1]  # 'sourceDir'
+        extension_files = sys.argv[2]  # 'mp4'
+        id_drive = sys.argv[3]  # 12lp12l4p125k12o215k
 
-now = datetime.date.today()
-two_m_ago = now - relativedelta(months=2)
+        now = datetime.date.today()
+        two_m_ago = now - relativedelta(months=2)
+        time_limit = time.mktime(two_m_ago.timetuple())
 
-time_limit = time.mktime(two_m_ago.timetuple())
+        files = Path(pathDirSource).glob('**/*.' + extension_files)
 
-# pathFull = pathDirSource+'/'+'*.'+extensionFiles
-# Searching files
-# files = glob.glob(pathFull)
-files = Path(pathDirSource).glob('**/*.' + extension_files)
+        if extension_files == 'txt':
+            opening_mode = 'r'
+        else:
+            opening_mode = 'rb'
 
-# print('files:', files)
+        for f in files:
+            if not files:
+                break
+            directories = str(f).rsplit('/', 1)[0].split('/')
+            file_name = str(f).rsplit('/', 1)[1]
 
-# if len(files) < 1:
-#    print("[WARN] Files not found!")
-# else:
-dic_path = {}
-for f in files:
+            # Continue if modified time is bigger than 2 months
+            mod_file_time = os.stat(f).st_mtime
+            mod_days = time.ctime(os.stat(f).st_mtime)
 
-    directories = str(f).rsplit('/', 1)[0].split('/')
-    # directories = str(file).rsplit('/', 1)[0].rsplit('/',1)[1]
+            if mod_file_time > time_limit:
+                continue
 
-    idParent = id_drive
-
-    file_name = str(f).rsplit('/', 1)[1]
-
-    if extension_files == 'txt':
-        opening_mode = 'r'
-    else:
-        opening_mode = 'rb'
-
-    # print('opening mode:', opening_mode)
-
-    # Continue if modified time is bigger than 2 months
-    mod_file_time = os.stat(f).st_mtime
-    mod_days = time.ctime(os.stat(f).st_mtime)
-
-    if mod_file_time > time_limit:
-        continue
-
-    print(f,
-          "| file date:", time.ctime(mod_file_time),
-          "| limit:", time.ctime(time_limit),
-          "| bigger:", mod_file_time > time_limit,
-          )
-    # print('filename:', file_name)
-
-    with open(str(f), opening_mode) as file_open:
-        try:
-            # print(opening_mode, file_name)
-            fn = os.path.basename(file_open.name)
-            file_drive = drive.CreateFile({'title': file_name,
-                                           'parents': [{"id": id_drive}]})
+            print("[INFO]",
+                  datetime.datetime.now(),
+                  "| file:", f,
+                  "| file date:", time.ctime(mod_file_time),
+                  "| limit:", time.ctime(time_limit),
+                  )
             if extension_files == 'txt':
-                file_drive.SetContentString(file_open.read())
+                with open(str(f), opening_mode) as file_open:
+                    file_drive = drive.CreateFile({'title': file_name,
+                                                   'parents': [{"id": id_drive}]})
+                    file_drive.SetContentString(file_open.read())
             else:
-                # print('f', file_open)
-                file_drive.SetContentFile(str(file_open.name))
+                file_drive = drive.CreateFile({'title': file_name,
+                                               'parents': [{"id": id_drive}]})
+                file_drive.SetContentFile(str(f))
             file_drive.Upload()
 
+            fn = os.path.basename(f)
             if file_drive["id"] is not None:
-                print("[OK] The file: " + fn + " has been uploaded.")
+                print("[INFO] The file: " + fn + " was uploaded.")
                 try:
                     os.remove(f)
-                    print("[OK] The file: " + fn + " has been removed of the hard disk.")
+                    print("[INFO] The file: " + fn + " was removed from the hard disk.")
                 except OSError as error:
                     print(error)
-                    print("[ERROR] The file: " + fn + " cannot be removed of the hard disk.")
+                    print("[ERROR] The file: " + fn + " was not removed from the hard disk.")
             else:
-                print("[ERROR] Upload faill to the file: " + fn + "!")
+                print("[ERROR] Upload failed to the file: " + fn + ". drive[id] not found")
+        break
+    except Exception as ex:
+        print(ex)
 
-        except Exception as ex:
-            print("[ERROR] Exception:", sys.exc_info()[0], "occured.")
-            raise
-
-# for removeDir in dicPath.keys():
-# shutil.rmtree(removeDir)
-
-print("-------------------------------")
-print("Finished!")
-print("-------------------------------")
+print("[END] Script was finished")
